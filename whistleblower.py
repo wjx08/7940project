9ee76120264607711b13d127e4ed238d8b49d48f
wjx08 <60465105+wjx08@users.noreply.github.com>
2020-03-16 17:00:04 +0800

    make it better

--- whistleblower.py
+++ whistleblower.py
@@ -29,6 +29,7 @@ app = Flask(__name__)
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+database_url = os.getenv('DATABASE_URL',None)
 
 # obtain the port that heroku assigned to this app.
 heroku_port = os.getenv('PORT', None)
@@ -38,6 +39,9 @@ if channel_secret is None:
     sys.exit(1)
 if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+if database_url is None:
+    print('Specify DATABASE_URL as environment variable.')
     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
